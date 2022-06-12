e058fb89202500471ebfbf0f4fcece17e348e9ab
Sakshi <70653660+Sakshi887220@users.noreply.github.com>
2022-06-12 16:24:43 +0530

    Update classifier.py

--- API/classifier.py
+++ API/classifier.py
@@ -6,11 +6,7 @@ from sklearn.linear_model import LogisticRegression
 from PIL import Image
 import PIL.ImageOps
 
-X, y = fetch_openml('mnist_784', version=1, return_X_y=True)
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=9, train_size=7500, test_size=2500)
-
-X_train_scaled = X_train/255.0
 X_test_scaled = X_test/255.0
 
 clf = LogisticRegression(solver='saga', multi_class='multinomial').fit(X_train_scaled, y_train)
